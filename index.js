1eedcc3a3ec5cd82f229b76404f6ef55ad93ba95
Vinayak Jaiswal <vinayakjaiswal121@gmail.com>
2021-02-20 09:22:11 +0530

    'adding-modules'

--- index.js
+++ index.js
@@ -2,20 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
+const replaceTemplate = require("./modules/replacrTemplate");
 
 const templateOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
